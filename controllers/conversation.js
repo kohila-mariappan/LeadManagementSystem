22ca9d635830e5644d8da3bb14b9a690e2363037
Kohila <kohila18.96@gmail.com>
2024-06-12 13:30:49 +0530

    Add sms send and histroy api

--- controllers/conversation.js
+++ controllers/conversation.js
@@ -1,5 +1,4 @@
 const db = require('../dbConfig/dbConnection.js')
-//const config = require('../config.js/dbConfig.js')
 const { default: Sequelize } = require('@sequelize/core')
 const statusCode = require('../utils/statusCode.js')
 const request = require('request')
@@ -15,9 +14,11 @@ const SendEmail = async(req,res) =>{
             let EmailLog = await ActivityLog(FromMail,ToMail,Subject,Content,CC,Bcc,SendEmail)
         }else{
             statusCode.successResponse(res,SendEmail)
+            let EmailLog = await ActivityLog(FromMail,ToMail,Subject,Content,CC,Bcc,SendEmail)
         }
     }catch(err){
-        statusCode.errorResponse(res,err)
+        let msg = err + " " + 'Failed to send Email'
+        statusCode.errorResponse(res,msg)
     }
 }
 
@@ -94,10 +95,99 @@ let EmailHistoryList = async(email) =>{
         console.log("DB Error")
         return err
     }
+}
+
+let SendSms = async(req,res) =>{
+    try{
+    let {SenderNo,ReceiverNo,Content} = req.body
+    let SmsStatus = await sendSmsToUser(SenderNo,ReceiverNo,Content)
+    if(SmsStatus === 'Success'){
+        statusCode.successResponse(res,SmsStatus)
+        let log = await SMSActivityLog(SenderNo,ReceiverNo,Content,SmsStatus)
+        //let EmailLog = await ActivityLog(FromMail,ToMail,Subject,Content,CC,Bcc,SendEmail)
+    }else{
+        statusCode.successResponse(res,SmsStatus)
+        let log = await SMSActivityLog(SenderNo,ReceiverNo,Content,SmsStatus)
+    }
+    }catch(err){
+        let msg = err + " " + 'Failed To Send SMS'
+        statusCode.errorResponse(res,msg)
+    }
+}
+
+let sendSmsToUser = async(SenderNo,ReceiverNo,Content) =>{
+    try{
+        const request = require('request');
+        const options = {
+            method: 'POST',
+            url: 'http://localhost:7027/api/sms/twilio',
+            headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: 'Basic Y29tbXVuaWNhdGlvbi1zZXJ2aWNlOnJyNlA2SG9MclFJT2psM0loMU5yXw=='
+                },
+                body: {body: `${Content}`, to: `${ReceiverNo}`},
+                json: true
+            };
+            const response = await new Promise((resolve, reject) => {
+                request(options, function (error, res, body) {
+                    if (error) {
+                        console.log('error',error)
+                        return reject(error);
+                    }
+                    console.log('success body',body)
+                    resolve(body); // Assuming response structure
+                  });
+            })
+            const SmsStatus = !response.sid ? 'Failed' :'Success'  ;
+            //console.log('data:', SmsStatus);
+            return SmsStatus
+    }catch(err){
+        return err
+    }
+}
+
+let SMSActivityLog= async(SenderNo,ReceiverNo,Content,SmsStatus) =>{
+    try{
+        let data = await db.sequelize.query("EXEC InsertSMSActivityLog @send = '"+SenderNo+"', @rec = '"+ReceiverNo+"', @con = '"+Content+"', @stat = '"+SmsStatus+"' ",{
+            type: Sequelize.QueryTypes.RAW})
+            return data
+    }catch(err){
+        console.log("DB Error")
+        return err
+    }
 
 }
- 
+
+let SMSHistory = async(req,res) =>{
+    try{
+        let {ReceiverNo} = req.body
+        let data = await UserSMSHistory(ReceiverNo)
+        if(data.length>0){
+            let msg = " SMS History"
+            statusCode.successResponseWithData(res,msg,data)
+        }else{
+            let msg = 'SMS History Not Exist For this Receiver'
+            statusCode.successResponse(res,msg)
+        }
+    }catch(err){
+        let msg = err + " " +'Error from view SMS History List'
+        statusCode.errorResponse(res,err)
+    }
+}
+
+let UserSMSHistory = async(Recevier) =>{
+    try{
+        let data = await db.sequelize.query("EXEC SMSHistroy @rec = '"+Recevier+"' ",{
+            type: Sequelize.QueryTypes.RAW})
+            return data[0]
+    }catch(err){
+        console.log("DB Error")
+        return err
+    }
+}
 module.exports = {
     SendEmail,
-    EmailHistory
+    EmailHistory,
+    SendSms,
+    SMSHistory
 }
--- routes/conversation.js
+++ routes/conversation.js
@@ -5,5 +5,7 @@ const ConController = require('../controllers/conversation')
 
 router.post('/email',ConController.SendEmail)
 router.post('/email/history',ConController.EmailHistory)
+router.post('/sms',ConController.SendSms)
+router.post('/sms/history',ConController.SMSHistory)
 
 module.exports = router
